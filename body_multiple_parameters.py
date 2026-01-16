8f1d5f2efbd533a0aa56a7ea7beb973f1ae2a9a4
G-Arun-Kumar <arun26.net@gmail.com>
2026-01-16 20:06:56 +0530

    modified multiple body parameters in 'body multiple parameters' file

--- body_multiple_parameters.py
+++ body_multiple_parameters.py
@@ -6,7 +6,7 @@ First, of course, you can mix Path, Query and request body parameter declaration
 
 And you can also declare body parameters as optional, by setting the default to None:"""
 
-from fastapi import FastAPI, Path, Query
+from fastapi import FastAPI, Path, Body
 from pydantic import BaseModel, Field
 from typing import Annotated, Literal
 
@@ -15,18 +15,16 @@ app = FastAPI()
 async def get_root():
     return {"message": "Hello Body-Multi Parameters"}
 
-class MultipleParameters(BaseModel):
-    limit: int = Field(100, gt=0, le=100)
-    offset: int = Field(0, le=100)
-    order_by: Literal["created_at", "updated_at"] = "created_at"
-    tags: list[str] = []
+class Items(BaseModel):
+    item_id: int
+    item_name: str | None = None
 
 @app.put("/multiparams/{params_id}")
 async def get_params(params_id: Annotated[str, Path(title="Multiple Parameters",
                                                     description="This is a multi parameter model",
                                                     min_length=3,
                                                     max_length=6)],
-                     multiparam: MultipleParameters | None = None,
+                     multiparam: Items | None = None,
                      q: str | None = None):
     final_items = {"Params ID" : params_id}
     if q:
@@ -39,18 +37,33 @@ async def get_params(params_id: Annotated[str, Path(title="Multiple Parameters",
 In the previous example, the path operations would expect a JSON body with the attributes of an Item, like:
 
 {
-    "limit": "25",
-    "offset": "25",
-    "order_by": "updated_at",
-    "tags": ["Arun", "Murugan"]
+    "item_id": "6",
+    "item_name": "Gugan",
 }
-But you can also declare multiple body parameters, e.g. multiparam and user:
+But you can also declare multiple body parameters, e.g. item and user:
 """
 class UserDetails(BaseModel):
     emp_id: int
     name: str | None = None
 
 @app.put("/users/{user_id}")
-async def user_details(user_id: UserDetails):
-    results = {"User ID" : user_id, "Multiple Params" : MultipleParameters, "Users" : UserDetails}
+async def user_details(user_id: int, item: Items, user: UserDetails):
+    results = {"User ID" : user_id, "Item Details" : item, "Users" : user}
     return results
+
+"""Singular values in body¶
+The same way there is a Query and Path to define extra data for query and path parameters, FastAPI provides an equivalent 'Body'.
+
+For example, extending the previous model, you could decide that you want to have another key
+'importance' in the same body, besides the item and user.
+
+If you declare it as is, because it is a singular value, FastAPI will assume that it is a query parameter.
+
+But you can instruct FastAPI to treat it as another body key using 'Body':
+Note: The 'body' method"""
+
+@app.put("/albums/{album_id}")
+async def get_albums(album_id: int, item_id: Items, user_id: UserDetails, importance: Annotated[str, Body()]):
+    results = {"Album Details" : album_id, "Item Details" : item_id, "User Details" : user_id, "Importance" : importance}
+    return results
+
