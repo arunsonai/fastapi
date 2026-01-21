76bb0eff1c329930a070306bda4d90968c6fedff
G-Arun-Kumar <arun26.net@gmail.com>
2026-01-21 10:29:41 +0530

    created cookie parameters file

---
+++ cookie_parameters.py
@@ -0,0 +1,18 @@
+"""Cookie Parameters¶
+You can define Cookie parameters the same way you define Query and Path parameters.
+
+Import Cookie¶
+First import Cookie:"""
+
+from fastapi import FastAPI, Cookie
+from typing import Annotated
+
+app = FastAPI()
+
+@app.get("/")
+async def get_root():
+    return {"message" : "Hello Cookie Parameters"}
+
+@app.get("/cookiedata/")
+async def get_data(cookieparam: Annotated[str | None, Cookie()] = None):
+    return {"Message" : cookieparam}
