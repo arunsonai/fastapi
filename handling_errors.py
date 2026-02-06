760823a6c617c281ff3335a5ee683c1de187249a
G-Arun-Kumar <arun26.net@gmail.com>
2026-02-06 22:54:33 +0530

    adding further code to handling errors

--- handling_errors.py
+++ handling_errors.py
@@ -12,3 +12,21 @@ async def get_users(user_id: str):
     if user_id not in items:
         raise HTTPException(status_code=404, detail="Information not available")
     return items[user_id]
+
+"""Add custom headers¶
+There are some situations in where it's useful to be able to add custom headers to the HTTP error.
+For example, for some types of security.
+
+You probably won't need to use it directly in your code.
+
+But in case you needed it for an advanced scenario, you can add custom headers:"""
+
+@app.get("/addheaders/{item_id}")
+async def add_headers(item_id: str):
+    if item_id not in items:
+        raise HTTPException(
+            status_code=404,
+            detail="Item not found",
+            headers={"X-Error" : "There goes my error"}
+        )
+    return {"Item Details" : items[item_id]}
