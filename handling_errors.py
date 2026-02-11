c7aa56c9aa9b3656f5edf80982a55c871ef219c4
G-Arun-Kumar <arun26.net@gmail.com>
2026-02-11 21:23:40 +0530

    added 'override exception handler' in handling errors file

--- handling_errors.py
+++ handling_errors.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI, HTTPException, Request
-from fastapi.responses import JSONResponse
+from fastapi.responses import JSONResponse, PlainTextResponse
+from fastapi.exceptions import RequestValidationError
+from starlette.exceptions import HTTPException as StarletteHTTPException
 
 app = FastAPI()
 
@@ -40,3 +42,46 @@ Let's say you have a custom exception UnicornException that you (or a library yo
 And you want to handle this exception globally with FastAPI.
 
 You could add a custom exception handler with @app.exception_handler():"""
+
+class Unicorn_Exception(Exception):
+    def __init__(self, name):
+        self.name = name
+
+# Remember to import 'Request' from fastapi and 'JSONRespinse' from 'fastapi.requests'
+@app.exception_handler(Unicorn_Exception)
+async def unicorn_exception_handler(request: Request, exc: Unicorn_Exception):
+    return JSONResponse(
+        status_code=418,
+        content={"message" : f"The {exc.name} done something wrong!"}
+    )
+
+@app.get("/exceptions/{name}")
+async def get_exception(name: str):
+    if name == "soorabadhman":
+        raise Unicorn_Exception(name=name)
+    return {"Unicorn_Name" : name}
+
+
+"""Override the default exception handlers¶
+FastAPI has some default exception handlers.
+
+These handlers are in charge of returning the default JSON responses when you raise an HTTPException and when the request has invalid data.
+
+You can override these exception handlers with your own.
+
+
+Override request validation exceptions¶
+When a request contains invalid data, FastAPI internally raises a RequestValidationError.
+
+And it also includes a default exception handler for it.
+
+To override it, import the RequestValidationError and use it with @app.exception_handler(RequestValidationError) to decorate the exception handler.
+
+The exception handler will receive a Request and the exception.
+"""
+
+@app.exception_handler(RequestValidationError)
+async def HTTP_Validation_Error(request, exc):
+    return PlainTextResponse(str(exc.detail), status_code = exc.status_code)
+
+@app.
