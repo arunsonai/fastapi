403a54440865612b5487df939c83054a9f516136
G-Arun-Kumar <arun26.net@gmail.com>
2026-03-05 08:04:05 +0530

    created dependency with yield

---
+++ dependencies_with_yield.py
@@ -0,0 +1,28 @@
+"""Dependencies with yield¶
+FastAPI supports dependencies that do some extra steps after finishing.
+
+To do this, use yield instead of return, and write the extra steps (code) after.
+
+Tip: Make sure to use yield one single time per dependency.
+
+Any function that is valid to use with:
+
+@contextlib.contextmanager or
+@contextlib.asynccontextmanager
+would be valid to use as a FastAPI dependency.
+
+In fact, FastAPI uses those two decorators internally."""
+
+
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+async def get_root():
+    return {"message" : "Hello Dependencies with yield!"}
+
+"""A database dependency with yield¶
+For example, you could use this to create a database session and close it after finishing.
+
+Only the code prior to and including the yield statement is executed before creating a response:"""
