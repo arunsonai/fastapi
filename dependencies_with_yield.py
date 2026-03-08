7214b211f77c6e2d52df4a2d2ecd86f6712e553e
G-Arun-Kumar <arun26.net@gmail.com>
2026-03-08 23:19:39 +0530

    modified the status code

--- dependencies_with_yield.py
+++ dependencies_with_yield.py
@@ -129,12 +129,12 @@ async def get_username():
     try:
         yield "Rick"
     except OwnerError as e:
-        raise HTTPException(status_code=400, detail=f"Owner Error : {e}")
+        raise HTTPException(status_code=404, detail=f"Owner Error : {e}")
     
 @app.get("/items/{item_id}")
 async def get_item(item_id: str, username: Annotated[str, Depends(get_username)]):
     if item_id not in data:
-        raise HTTPException(status_code=400, detail= "Item not exist")
+        raise HTTPException(status_code=404, detail= "Item not exist")
     item = data[item_id]
     if item["owner"] != username:
         raise OwnerError(username)
